e57d99e9a62d3808903deae1e7e07e6b29919452
Chris Tran <tranduckhanh96@gmail.com>
2024-03-11 00:33:40 -0600

    fix tailwind css and antd conflict

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ConfigProvider } from "antd";
+import { StyleProvider } from "@ant-design/cssinjs";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider>
+      <StyleProvider hashPriority="high">
+        <App />
+      </StyleProvider>
+    </ConfigProvider>
+  </React.StrictMode>
+);
